8469fae9a929d9eceaeb36a15d202f177c882db0
Ashley <47555119+littleolme@users.noreply.github.com>
2019-03-27 16:42:15 +1100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+</body>
+
+    <footer>
+
+     <link rel="stylesheet" href="assets/css/style.css">    
+        
+        <p>Created by Ashley McGrath</p>
+        <p>Student ID: u3175611</p>  
+        
+    </footer>
+
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    
+    <title>Habitual</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+    <h1>It's Habitual</h1>
+    
+        <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="create.php">Add a New Task</a></li>
+        <li><a href="read.php">List All Tasks</a></li>
+        <li><a href="update.php">Edit an Task</a></li>
+        <li><a href="delete.php">Delete an Task</a></li>
+    </ul>
