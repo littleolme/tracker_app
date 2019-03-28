fc8f7aa7d1d70be416a31faa2eebb5706d5134a7
Ashley <47555119+littleolme@users.noreply.github.com>
2019-03-28 17:02:06 +1100

    Update update.php

--- update.php
+++ update.php
@@ -1,24 +1,17 @@
 <?php 
 	
-    // include the config file that we created before
     require "../config.php"; 
     
-    // this is called a try/catch statement 
 	try {
-        // FIRST: Connect to the database
         $connection = new PDO($dsn, $username, $password, $options);
 		
-        // SECOND: Create the SQL 
         $sql = "SELECT * FROM tasks";
         
-        // THIRD: Prepare the SQL
         $statement = $connection->prepare($sql);
         $statement->execute();
         
-        // FOURTH: Put it into a $result object that we can access in the page
         $result = $statement->fetchAll();
 	} catch(PDOException $error) {
-        // if there is an error, tell us what it is
 		echo $sql . "<br>" . $error->getMessage();
 	}	
 ?>
@@ -28,7 +21,6 @@
 
 <h2>Results</h2>
 
-<!-- This is a loop, which will loop through each result in the array -->
 <?php foreach($result as $row) { ?>
 
 <p>
@@ -42,7 +34,7 @@
 </p>
 
 <hr>
-<?php }; //close the foreach
+<?php };
 ?>
 
 
