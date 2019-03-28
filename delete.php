7f1aa72994217056803bab0b5ade6ec395b66992
Ashley <47555119+littleolme@users.noreply.github.com>
2019-03-28 16:55:17 +1100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,32 +1,24 @@
 <?php 
 	
-  // include the config file 
     require "../config.php";
     require "common.php";
 
-    // This code will only run if the delete button is clicked
     if (isset($_GET["id"])) {
-	    // this is called a try/catch statement 
+
         try {
-            // define database connection
+
             $connection = new PDO($dsn, $username, $password, $options);
             
-            // set id variable
             $id = $_GET["id"];
             
-            // Create the SQL 
             $sql = "DELETE FROM tasks WHERE id = :id";
 
-            // Prepare the SQL
             $statement = $connection->prepare($sql);
             
-            // bind the id to the PDO
             $statement->bindValue(':id', $id);
             
-            // execute the statement
             $statement->execute();
-
-            // Success message
+		
             $success = "task successfully deleted";
 
         } catch(PDOException $error) {
@@ -35,18 +27,14 @@
         }
     };
 
-    // This code runs on page load
     try {
         $connection = new PDO($dsn, $username, $password, $options);
 		
-        // SECOND: Create the SQL 
         $sql = "SELECT * FROM tasks";
         
-        // THIRD: Prepare the SQL
         $statement = $connection->prepare($sql);
         $statement->execute();
         
-        // FOURTH: Put it into a $result object that we can access in the page
         $result = $statement->fetchAll();
     } catch(PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
@@ -58,7 +46,6 @@
 
 <h2>Results</h2>
 
-<!-- This is a loop, which will loop through each result in the array -->
 <?php foreach($result as $row) { ?>
 
 <p>
@@ -72,7 +59,7 @@
 </p>
 
 <hr>
-<?php }; //close the foreach
+<?php }; 
 ?>
 
 
