577c320de8703e75ea2e1601e5b794e4c2beb707
Ashley <47555119+littleolme@users.noreply.github.com>
2019-03-28 17:01:21 +1100

    Update update-task.php

--- update-task.php
+++ update-task.php
@@ -1,13 +1,12 @@
 <?php 
-    // include the config file that we created last week
+
     require "../config.php";
     require "common.php";
-    // run when submit button is clicked
+
     if (isset($_POST['submit'])) {
         try {
             $connection = new PDO($dsn, $username, $password, $options);  
             
-            //grab elements from form and set as varaible
             $task =[
               "id"         => $_POST['id'],
               "taskname" => $_POST['taskname'],
@@ -17,7 +16,6 @@
               "date"   => $_POST['date'],
             ];
             
-            // create SQL statement
             $sql = "UPDATE `tasks` 
                     SET id = :id, 
                         taskname = :taskname, 
@@ -26,47 +24,37 @@
                         tasknotes = :tasknotes, 
                         date = :date 
                     WHERE id = :id";
-            //prepare sql statement
+
             $statement = $connection->prepare($sql);
             
-            //execute sql statement
+
             $statement->execute($task);
         } catch(PDOException $error) {
             echo $sql . "<br>" . $error->getMessage();
         }
     }
-    // GET data from DB
-    //simple if/else statement to check if the id is available
+
     if (isset($_GET['id'])) {
-        //yes the id exists 
         
         try {
-            // standard db connection
             $connection = new PDO($dsn, $username, $password, $options);
             
-            // set if as variable
             $id = $_GET['id'];
             
-            //select statement to get the right data
             $sql = "SELECT * FROM tasks WHERE id = :id";
             
-            // prepare the connection
             $statement = $connection->prepare($sql);
             
-            //bind the id to the PDO id
             $statement->bindValue(':id', $id);
             
-            // now execute the statement
             $statement->execute();
             
-            // attach the sql statement to the new task variable so we can access it in the form
             $task = $statement->fetch(PDO::FETCH_ASSOC);
             
         } catch(PDOExcpetion $error) {
             echo $sql . "<br>" . $error->getMessage();
         }
     } else {
-        // no id, show error
         echo "No id - something went wrong";
         //exit;
     };
