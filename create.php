2933250e1f5bf8e5c0e271fc623e1e0cd56ac297
Ashley <47555119+littleolme@users.noreply.github.com>
2019-03-28 16:51:53 +1100

    Update create.php

--- create.php
+++ create.php
@@ -1,17 +1,13 @@
 <?php 
 
-// this code will only execute after the submit button is clicked
 if (isset($_POST['submit'])) {
 	
-    // include the config file that we created before
     require "../config.php"; 
     
-    // this is called a try/catch statement 
 	try {
-        // FIRST: Connect to the database
+
         $connection = new PDO($dsn, $username, $password, $options);
-		
-        // SECOND: Get the contents of the form and store it in an array
+
         $new_task = array( 
             "taskname" => $_POST['taskname'], 
             "taskdate" => $_POST['taskdate'],
@@ -19,15 +15,12 @@ if (isset($_POST['submit'])) {
             "tasknotes" => $_POST['tasknotes'], 
         );
         
-        // THIRD: Turn the array into a SQL statement
         $sql = "INSERT INTO tasks (taskname, taskdate, tasktime, tasknotes) VALUES (:taskname, :taskdate, :tasktime, :tasknotes)";        
         
-        // FOURTH: Now write the SQL to the database
         $statement = $connection->prepare($sql);
         $statement->execute($new_task);
         
 	} catch(PDOException $error) {
-        // if there is an error, tell us what it is
 		echo $sql . "<br>" . $error->getMessage();
 	}	
 }
@@ -41,8 +34,6 @@ if (isset($_POST['submit'])) {
 <?php if (isset($_POST['submit']) && $statement) { ?>
 <p>task successfully added!</p>
 <?php } ?>
-
-<!--form to collect data for each artwork-->
 
 <form method="post">
     <label for="taskname">Task Name</label>
